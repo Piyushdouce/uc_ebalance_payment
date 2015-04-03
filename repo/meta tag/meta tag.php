18039c4d1d8cd79c111a020a34ddd97a5562353f
piyushdouce <piyush.drupalchamp@gmail.com>
2015-04-03 17:03:06 +0530

    module added

---
+++ repo/meta tag/meta tag.php
@@ -0,0 +1,27 @@
+<h1>Meta Tags to Drupal Content</h1>
+<p>Depending on how far along your company is on SEO, you may need to write a basic strategy first, and then revisit it after doing some work to audit and analyze your current SEO efforts and data (more on that later!). Search engine optimization (SEO) is often at the top of the to-do list when planning a site.</p>
+<p>&lt;meta&gt; tag helps your pages rise to the top of a search engine result.</p>
+<p>In this blog, we'll explore the Meta tags module, to add metadata to your nodes, terms, and users.</p>
+<h3>1. Installation</h3>
+<p>Install the module <a target="_blank" href="https://www.drupal.org/project/metatag">metatag</a></p>
+<h3>2. Configuration</h3>
+<p>The Meta Tags module comes already configured but you can make changes.</p>
+<p>Lets configure the meta tags for the Blog content type.</p>
+<ul>
+  <ul>
+    <li>Click on Add a meta tag default<br />
+      <p><img src="/sites/default/files/u127/add_default_metatag.png" /></p>
+    </li>
+    <li>Select Blog from the Type dropdown.<br />
+      <p><img src="/sites/default/files/u127/select%20content%20type%20or%20taxo.png" /></p>
+    </li>
+    <li>Enter the token [node:content-type:description] in the abstract field and save<br />
+      <p><img src="/sites/default/files/u127/add_abstract.png" /></p>
+    </li>
+  </ul>
+</ul>
+<h3>Add metatag for individual node</h3>
+<p>Go to Add content and add a Blog(Your content type name). Scroll down and click on the Meta tags tab.</p>
+<p><img src="/sites/default/files/u127/add_metadata_for_individual_node.png" /></p>
+<h3>Add metatag for Taxonomy term</h3>
+<p>The process is the same when setting up meta data for vocabulary terms and users.</p>
